999fe9a870dac1df59a42bba29c0202e2972900b
jrnazareno <nazareno.j.rouel@gmail.com>
2021-11-04 08:46:35 +0800

    added kde-applications

--- install-arch-linux-plasma.sh
+++ install-arch-linux-plasma.sh
@@ -81,7 +81,7 @@ GPU="mesa"
 
 #Defines the additional packages to install (such as useful packages for the system, Desktop environment, display manager, etc... Don't forget to modify the "systemctl enable" part depending on your choices.
 PACKAGES() {
-	pacman -S --noconfirm --needed networkmanager vim base-devel linux-headers bash-completion xorg plasma sddm > /dev/null 2>&1 && systemctl enable NetworkManager > /dev/null 2>&1 && systemctl enable gdm > /dev/null 2>&1
+	pacman -S --noconfirm --needed networkmanager vim base-devel linux-headers bash-completion xorg plasma sddm kde-applications > /dev/null 2>&1 && systemctl enable NetworkManager > /dev/null 2>&1 && systemctl enable gdm > /dev/null 2>&1
 }
 
 #################################################################################################################
